23e1f6148601b7bd4c7a6e6626ae296f764c7d16
Hassan <hassan.sweden.dev@gmail.com>
2023-05-14 16:03:24 +0200

    finish featured

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import heroManImage from "./image/man.png";
+import heroSearchIcon from "./image/search.png";
+
+const assetsData = {
+  images: {
+    heroManImage
+  },
+  icons: {
+    heroSearchIcon
+  }
+};
+
+export default assetsData;
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,29 +1,61 @@
+import assetsData from "../../assets";
+
+const { heroManImage } = assetsData.images;
+const { heroSearchIcon } = assetsData.icons;
+
 import "./Featured.scss";
 
 const Featured = () => {
   return (
     <section className="featured">
-      <div className="left">
-        <h1>Find the perfect freelance services for your business</h1>
-        <div className="search">
-          <div className="search-input">
-            <img src="" alt="" />
-            <input type="text" placeholder="Try building mobile app" />
+      <div className="container d-flex align-items-center">
+        <div className="left d-flex flex-column gap-4">
+          <h1 className="h1 mb-0">
+            Find the perfect <i className="hl">freelance</i> services for your
+            business
+          </h1>
+
+          <div
+            className="d-flex justify-content-between bg-white rounded-2"
+            role="Search">
+            <div
+              role="Search Input"
+              className="search-input d-flex align-items-center gap-2">
+              <img
+                className="search-icon m-3"
+                src={heroSearchIcon}
+                alt="A search icon"
+              />
+              <input
+                className="border-0 outline-0"
+                type="text"
+                placeholder="Try building mobile app"
+              />
+            </div>
+
+            <button className="btn--featured">Search</button>
           </div>
 
-          <button>Search</button>
+          <div className="popular d-flex align-items-center gap-3">
+            <span>Popular:</span>
+            <button className="btn--popular btn-sm rounded-3 py-1 px-3 rounded-pill">
+              Web Design
+            </button>
+            <button className="btn--popular btn-sm rounded-3 py-1 px-3 rounded-pill">
+              Wordpress
+            </button>
+            <button className="btn--popular btn-sm rounded-3 py-1 px-3 rounded-pill">
+              Logo Design
+            </button>
+            <button className="btn--popular btn-sm rounded-3 py-1 px-3 rounded-pill">
+              AI Services
+            </button>
+          </div>
         </div>
 
-        <div className="popular">
-          <span>Popular</span>
-          <button>Web Design</button>
-          <button>Wordpress</button>
-          <button>Logo Design</button>
-          <button>AI Services</button>
+        <div className="right">
+          <img src={heroManImage} alt="A Man Image" />
         </div>
-      </div>
-      <div className="right">
-        <h2>Right</h2>
       </div>
     </section>
   );
